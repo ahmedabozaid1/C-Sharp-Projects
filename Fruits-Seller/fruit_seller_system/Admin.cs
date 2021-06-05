5403ff891b18ffeb669fa8eabb80fc782d8bdcfc
Ahmed Abozaid <73134650+ahmedabozaid1@users.noreply.github.com>
2021-06-05 02:05:12 +0200

    Add files via upload

---
+++ Fruits-Seller/fruit_seller_system/Admin.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fruit_seller_system
+{
+    public partial class Adminwindow : Form
+    {
+        public Adminwindow()
+        {
+            InitializeComponent();
+        }
+      
+        string path = "/";
+        int view = 0;
+        List<fruits> LF = new List<fruits>();
+        private void uploadimageBT_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OFD = new OpenFileDialog();
+            if (OFD.ShowDialog() == DialogResult.OK)
+            {
+                path = OFD.FileName;
+            }
+        }
+        private void additemBT_Click(object sender, EventArgs e)
+        {
+            if (nameTB.Text != "" && priceTB.Text != "" && colorTB.Text != "")
+            {
+                StreamWriter SW = new StreamWriter("fruits.txt", true);
+                string name = nameTB.Text;
+                string price = priceTB.Text;
+                string color = colorTB.Text;
+                if (path == "/")
+                {
+                    MessageBox.Show("please enter The image");
+                }
+                else
+                {
+                   
+                    SW.WriteLine(name+ "," + price + "," + path + "," + color);
+                    nameTB.Text = "";
+                    priceTB.Text = "";
+                    colorTB.Text = "";
+                }
+                SW.Close();
+            }
+        }
+
+        private void Adminwindow_Load(object sender, EventArgs e)
+        {
+            StreamReader SR = new StreamReader("fruits.txt");
+            while (!SR.EndOfStream)
+            {
+                fruits pnn = new fruits();
+                string line = SR.ReadLine();
+                string[] temp = line.Split(',');
+                pnn.name = temp[0];
+                pnn.price = temp[1];
+                pnn.image = temp[2];
+                pnn.color = temp[3];
+                LF.Add(pnn);
+            }
+            SR.Close();
+
+            pictureBox1.Image = Image.FromFile(LF[0].image);
+            pricevalueLB.Text = LF[0].price;
+            fruitnameLB.Text = LF[0].name;
+            this.BackColor = Color.FromName(LF[0].color);
+
+        }
+
+        private void nextBT_Click(object sender, EventArgs e)
+        {
+            if(view!=LF.Count-1)
+            {
+                view++;
+            }
+            else
+            {
+                view = 0;
+            }
+           
+            pictureBox1.Image = Image.FromFile(LF[view].image);
+            pricevalueLB.Text = LF[view].price;
+            fruitnameLB.Text = LF[view].name;
+            this.BackColor=Color.FromName(LF[view].color);
+        }
+
+        private void previousBT_Click(object sender, EventArgs e)
+        {
+            if(view==0)
+            {
+                view = LF.Count - 1;
+            }
+            else
+            {
+                view--;
+            }
+            pictureBox1.Image = Image.FromFile(LF[view].image);
+            pricevalueLB.Text = LF[view].price;
+            fruitnameLB.Text = LF[view].name;
+            this.BackColor = Color.FromName(LF[view].color);
+        }
+    }
+    public class fruits
+    {
+        public string name;
+        public string price;
+        public string color;
+        public string image;
+        public int count = 1;
+    }
+}
---
+++ Fruits-Seller/fruit_seller_system/NewUser.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace fruit_seller_system
+{
+    partial class NewUserwindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usernameTB = new System.Windows.Forms.TextBox();
+            this.signinlabel = new System.Windows.Forms.Label();
+            this.passwordTB = new System.Windows.Forms.TextBox();
+            this.usernameLB = new System.Windows.Forms.Label();
+            this.passwordLB = new System.Windows.Forms.Label();
+            this.verifyBT = new System.Windows.Forms.TextBox();
+            this.verifypasswordLB = new System.Windows.Forms.Label();
+            this.createBT = new System.Windows.Forms.Button();
+            this.AgreecheckBox = new System.Windows.Forms.CheckBox();
+            this.TermsandConditionsLink = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // usernameTB
+            // 
+            this.usernameTB.BackColor = System.Drawing.Color.Azure;
+            this.usernameTB.Location = new System.Drawing.Point(250, 134);
+            this.usernameTB.Name = "usernameTB";
+            this.usernameTB.Size = new System.Drawing.Size(180, 20);
+            this.usernameTB.TabIndex = 3;
+            // 
+            // signinlabel
+            // 
+            this.signinlabel.AutoSize = true;
+            this.signinlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signinlabel.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.signinlabel.Location = new System.Drawing.Point(153, 38);
+            this.signinlabel.Name = "signinlabel";
+            this.signinlabel.Size = new System.Drawing.Size(277, 39);
+            this.signinlabel.TabIndex = 4;
+            this.signinlabel.Text = "Creat New User";
+            // 
+            // passwordTB
+            // 
+            this.passwordTB.BackColor = System.Drawing.Color.Azure;
+            this.passwordTB.Location = new System.Drawing.Point(250, 174);
+            this.passwordTB.Name = "passwordTB";
+            this.passwordTB.PasswordChar = '*';
+            this.passwordTB.Size = new System.Drawing.Size(180, 20);
+            this.passwordTB.TabIndex = 5;
+            // 
+            // usernameLB
+            // 
+            this.usernameLB.AutoSize = true;
+            this.usernameLB.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.usernameLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameLB.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.usernameLB.Location = new System.Drawing.Point(141, 130);
+            this.usernameLB.Name = "usernameLB";
+            this.usernameLB.Size = new System.Drawing.Size(103, 24);
+            this.usernameLB.TabIndex = 6;
+            this.usernameLB.Text = "username";
+            // 
+            // passwordLB
+            // 
+            this.passwordLB.AutoSize = true;
+            this.passwordLB.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.passwordLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordLB.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.passwordLB.Location = new System.Drawing.Point(145, 169);
+            this.passwordLB.Name = "passwordLB";
+            this.passwordLB.Size = new System.Drawing.Size(99, 24);
+            this.passwordLB.TabIndex = 7;
+            this.passwordLB.Text = "password";
+            // 
+            // verifyBT
+            // 
+            this.verifyBT.BackColor = System.Drawing.Color.Azure;
+            this.verifyBT.Location = new System.Drawing.Point(250, 211);
+            this.verifyBT.Name = "verifyBT";
+            this.verifyBT.PasswordChar = '*';
+            this.verifyBT.Size = new System.Drawing.Size(180, 20);
+            this.verifyBT.TabIndex = 8;
+            // 
+            // verifypasswordLB
+            // 
+            this.verifypasswordLB.AutoSize = true;
+            this.verifypasswordLB.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.verifypasswordLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.verifypasswordLB.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.verifypasswordLB.Location = new System.Drawing.Point(90, 206);
+            this.verifypasswordLB.Name = "verifypasswordLB";
+            this.verifypasswordLB.Size = new System.Drawing.Size(154, 24);
+            this.verifypasswordLB.TabIndex = 9;
+            this.verifypasswordLB.Text = "verify password";
+            // 
+            // createBT
+            // 
+            this.createBT.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.createBT.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.createBT.Enabled = false;
+            this.createBT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.createBT.Location = new System.Drawing.Point(325, 291);
+            this.createBT.Name = "createBT";
+            this.createBT.Size = new System.Drawing.Size(114, 42);
+            this.createBT.TabIndex = 10;
+            this.createBT.Text = "Create";
+            this.createBT.UseVisualStyleBackColor = false;
+            this.createBT.Click += new System.EventHandler(this.createBT_Click);
+            // 
+            // AgreecheckBox
+            // 
+            this.AgreecheckBox.AutoSize = true;
+            this.AgreecheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AgreecheckBox.Location = new System.Drawing.Point(84, 303);
+            this.AgreecheckBox.Name = "AgreecheckBox";
+            this.AgreecheckBox.Size = new System.Drawing.Size(100, 20);
+            this.AgreecheckBox.TabIndex = 13;
+            this.AgreecheckBox.Text = "Agree to our";
+            this.AgreecheckBox.UseVisualStyleBackColor = true;
+            this.AgreecheckBox.CheckedChanged += new System.EventHandler(this.AgreecheckBox_CheckedChanged);
+            // 
+            // TermsandConditionsLink
+            // 
+            this.TermsandConditionsLink.AutoSize = true;
+            this.TermsandConditionsLink.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TermsandConditionsLink.Location = new System.Drawing.Point(180, 304);
+            this.TermsandConditionsLink.Name = "TermsandConditionsLink";
+            this.TermsandConditionsLink.Size = new System.Drawing.Size(139, 16);
+            this.TermsandConditionsLink.TabIndex = 26;
+            this.TermsandConditionsLink.TabStop = true;
+            this.TermsandConditionsLink.Text = "Terms and Conditions";
+            // 
+            // NewUserwindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(596, 410);
+            this.Controls.Add(this.TermsandConditionsLink);
+            this.Controls.Add(this.AgreecheckBox);
+            this.Controls.Add(this.createBT);
+            this.Controls.Add(this.verifypasswordLB);
+            this.Controls.Add(this.verifyBT);
+            this.Controls.Add(this.passwordLB);
+            this.Controls.Add(this.usernameLB);
+            this.Controls.Add(this.passwordTB);
+            this.Controls.Add(this.signinlabel);
+            this.Controls.Add(this.usernameTB);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "NewUserwindow";
+            this.Text = "New User";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox usernameTB;
+        private System.Windows.Forms.Label signinlabel;
+        private System.Windows.Forms.TextBox passwordTB;
+        private System.Windows.Forms.Label usernameLB;
+        private System.Windows.Forms.Label passwordLB;
+        private System.Windows.Forms.TextBox verifyBT;
+        private System.Windows.Forms.Label verifypasswordLB;
+        private System.Windows.Forms.Button createBT;
+        private System.Windows.Forms.CheckBox AgreecheckBox;
+        private System.Windows.Forms.LinkLabel TermsandConditionsLink;
+    }
+}
